3fe0024452ec6445a72341823840dbaf41ea674a
sojebsikder <sojebsikder@gmail.com>
2022-01-08 15:11:54 +0600

    main

--- resources/views/app/cart.blade.php
+++ resources/views/app/cart.blade.php
@@ -6,7 +6,44 @@
 <div class="container" style="margin-top: 80px">
     <h2>Cart Page</h2>
 
-    
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Product Name</th>
+                <th scope="col">Product Description</th>
+                <th scope="col">Brand</th>
+                <th scope="col">Qnty</th>
+                <th scope="col">Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+            $id = 0;
+            @endphp
+
+            @forelse ($data as $row)
+
+            @php
+            $id++;
+            @endphp
+
+            <tr>
+                <th scope="row">{{ $id }}</th>
+                <td> {{ $row->products->title }} </td>
+                <td> {{ $row->products->content }} </td>
+                <td> {{ $row->products->brand }} </td>
+                <td> {{ $row->qnty }} </td>
+                <td> {{ $row->products->price }}tk </td>
+            </tr>
+
+            @empty
+            <p>No product</p>
+            @endforelse
+        </tbody>
+    </table>
+    <hr>
+    Total Price:
 </div>
 
 @extends('app\partial\footer\footer')
