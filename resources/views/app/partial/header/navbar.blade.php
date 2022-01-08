723fb4d8baa3b902af8228f90350048adb6b7780
sojebsikder <sojebsikder@gmail.com>
2022-01-08 14:11:45 +0600

    main

--- resources/views/app/partial/header/navbar.blade.php
+++ resources/views/app/partial/header/navbar.blade.php
@@ -1,6 +1,6 @@
     <!-- Navbar start -->
     <nav class="navbar navbar-expand-lg navbar-light bg-info fixed-top">
-        <a class="navbar-brand" href="/">Navbar</a>
+        <a class="navbar-brand" href="/"> {{ env('APP_NAME') }}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
