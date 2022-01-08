e0ad76c7acdef9db848ba9ef99559df79979487e
sojebsikder <sojebsikder@gmail.com>
2022-01-08 14:29:20 +0600

    main

--- resources/views/app/product_details.blade.php
+++ resources/views/app/product_details.blade.php
@@ -8,16 +8,20 @@
 
 
         <!-- product card start -->
-        <div class="card" style="width: 18rem">
+        <div class="card w-50" style="width: 18rem">
             <img class="card-img-top" src=" {{ asset('uploads/products/'.$data->image) }}" alt="Card image cap" />
             <div class="card-body">
                 <h5 class="card-title"> {{ $data->title }} </h5>
                 <p class="card-text">{{ $data->content }}.</p>
 
-                <a href="#" class="btn btn-primary">Add to cart</a>
+                
             </div>
         </div>
         <!-- product card end -->
+        <div style="margin-left: 10px;">
+             <a href="#" class="btn btn-primary">Add to cart</a>
+        </div>
+       
     </div>
 </div>
 
