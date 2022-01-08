d605ba95b99e7779749b95ea5704809327914067
sojebsikder <sojebsikder@gmail.com>
2022-01-08 13:16:31 +0600

    main

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,8 @@ Route::group(['prefix' => 'admin'], function () {
 Route::get('/', [App\Http\Controllers\app\IndexController::class, 'index']);
 Route::get('/product/{id}', [App\Http\Controllers\app\IndexController::class, 'show']);
 // Auth
-Route::get('/profile', [App\Http\Controllers\app\AuthController::class, 'profile_page']);
-Route::get('/login', [App\Http\Controllers\app\AuthController::class, 'login_page']);
+Route::get('/profile', [App\Http\Controllers\app\AuthController::class, 'profile_page'])->middleware('auth');
+Route::get('/login', [App\Http\Controllers\app\AuthController::class, 'login_page'])->name('login');
 Route::post('/login', [App\Http\Controllers\app\AuthController::class, 'login']);
 Route::get('/register', [App\Http\Controllers\app\AuthController::class, 'register_page']);
 Route::post('/register', [App\Http\Controllers\app\AuthController::class, 'register']);
