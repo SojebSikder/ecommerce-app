ae65c691c95fb50a18598e714f76c7667df4595f
Sojeb Sikder <sojebsikder@gmail.com>
2022-05-18 16:35:05 +0600

    main

--- resources/views/app/partial/header/navbar.blade.php
+++ resources/views/app/partial/header/navbar.blade.php
@@ -6,41 +6,47 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                 <button class="btn btn-success my-2 my-sm-0" type="submit">
                     Search
                 </button>
             </form>
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 
-                        @if(session('username'))
-                        {{ session('username') }}
-                        @else
-                        Account
-                        @endif
+            <div class="mx-auto my-2 order-0 order-md-1 position-relative">
 
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        @if(session('username'))
-                        <a class="dropdown-item" href="/profile">Profile</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="{{ URL::to('logout') }}">Logout</a>
-                        @else
-                        <a class="dropdown-item" href="/login">Login</a>
-                        <a class="dropdown-item" href="/register">Register</a>
-                        @endif
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/cart"> <i class="fas fa-shopping-cart"></i> Cart</a>
-                </li>
-            </ul>
+
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+                            @if(session('username'))
+                            {{ session('username') }}
+                            @else
+                            Account
+                            @endif
+
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            @if(session('username'))
+                            <a class="dropdown-item" href="/profile">Profile</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ URL::to('logout') }}">Logout</a>
+                            @else
+                            <a class="dropdown-item" href="/login">Login</a>
+                            <a class="dropdown-item" href="/register">Register</a>
+                            @endif
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/cart"> <i class="fas fa-shopping-cart"></i> Cart</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
     <!-- Navbar end -->
